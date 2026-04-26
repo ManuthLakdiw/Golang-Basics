a57c570561a13bc01807a68ffe6a81145c205f47
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-04-26 16:05:14 +0530

    feat: implement range-based iteration examples and clean up loop syntax in main.go

--- loops/main.go
+++ loops/main.go
@@ -4,12 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Loops")
-	var number int = 10;
+	var number int = 10
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
-
 
 	// while
 	for number > 5 {
@@ -18,7 +17,22 @@ func main() {
 	}
 
 	// infinite loop
-	for {
-		fmt.Printf("my name is %s and i am %v years old\n", "Manuth", 19)
+	// for {
+	// 	fmt.Printf("my name is %s and i am %v years old\n", "Manuth", 19)
+	// }
+
+	// range
+	for i, v := range "Manuth" {
+		fmt.Println(i, v)
+	}
+
+	for v := range 5 {
+		fmt.Println(v)
+	}
+
+	arr := []int{1, 2, 3, 4, 5}
+
+	for i, v := range arr {
+		fmt.Println(i, v)
 	}
 }
