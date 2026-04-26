388e1229830135feed2aaeca99e8624a7d88542b
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-04-26 16:16:05 +0530

    feat: add control flow example with conditional if-else statements

---
+++ control_flow/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Control Flow")
+
+	age := 22
+
+    if age >= 18 {
+        fmt.Println("You are an adult.")
+    } else if age >= 13 {
+        fmt.Println("You are a teenager.")
+    } else {
+        fmt.Println("You are a child.")
+    }
+}
+
