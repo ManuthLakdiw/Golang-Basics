ebfefc812459d1383c6e95b985d45511c6a899f8
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-04-26 16:01:00 +0530

    feat: implement while loop logic in main function

--- loops/main.go
+++ loops/main.go
@@ -4,8 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Loops")
+	var number int = 10;
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+
+
+	// while
+	for number > 5 {
+		fmt.Println(number)
+		number--
+	}
+
+	
 }
