3c4dda9cd2b00e73a4717c01de8e5f2de43293c3
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-04-26 17:25:40 +0530

    feat: add anonymous function implementation and fix code indentation in main.go

--- functions/main.go
+++ functions/main.go
@@ -7,23 +7,26 @@ func main() {
 
 	displayMessage()
 
-		// Parameters / Arguments
+	// Parameters / Arguments
 
-	sum := add(5, 10) // 5 සහ 10 තමයි arguments
-	fmt.Println("Sum:", sum)     // Output: Sum: 15
+	sum := add(5, 10)        // 5 සහ 10 තමයි arguments
+	fmt.Println("Sum:", sum) // Output: Sum: 15
 
 	displayMessageWithParam("John") // John තමයි argument
 
-		// Multiple return Values
-    a, b := swap("Hello", "World")
-    fmt.Println("Swapped:", a, b) // Output: Swapped: World Hello
+	// Multiple return Values
+	a, b := swap("Hello", "World")
+	fmt.Println("Swapped:", a, b) // Output: Swapped: World Hello
 
-		// Variadic Functions
-    fmt.Println("Sum of 1, 2, 3:", sumAll(1, 2, 3)) // Output: 6
-    fmt.Println("Sum of 10, 20:", sumAll(10, 20))   // Output: 30
+	// Variadic Functions
+	fmt.Println("Sum of 1, 2, 3:", sumAll(1, 2, 3)) // Output: 6
+	fmt.Println("Sum of 10, 20:", sumAll(10, 20))   // Output: 30
 
-	
-
+	// Anonymous Function
+	addAnonymous := func(x int, y int) int {
+		return x + y
+	}
+	fmt.Println("Anonymous Sum:", addAnonymous(4, 5)) // Output: 9
 
 }
 
@@ -39,20 +42,18 @@ func displayMessageWithParam(name string) {
 	fmt.Println("Hello,", name, "Welcome to Functions!")
 }
 
-
 //// Multiple return Values
 
 func swap(x string, y string) (string, string) {
-    return y, x
+	return y, x
 }
-
 
 // variadic functions
 
 func sumAll(numbers ...int) int {
-    total := 0
-    for _, num := range numbers {
-        total += num
-    }
-    return total
+	total := 0
+	for _, num := range numbers {
+		total += num
+	}
+	return total
 }
