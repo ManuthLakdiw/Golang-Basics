9f1f72043b6b30499c58b71a93e08b30355c47a7
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-02 00:12:02 +0530

    refactor: optimize critical section access using mutexes in concurrency example

--- concurrency/mutex/main.go
+++ concurrency/mutex/main.go
@@ -1,4 +1,34 @@
-// without mutex - Data Race
+// // without mutex - Data Race
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// var counter int = 0 // හැමෝම පාවිච්චි කරන පොදු Variable එක
+
+// func increment(wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	// Goroutines 1000ක් එකම වෙලාවේ මේකට කඩාපනිනවා!
+// 	counter++
+// }
+
+// func main() {
+// 	var wg sync.WaitGroup
+
+// 	for i := 0; i < 1000; i++ {
+// 		wg.Add(1)
+// 		go increment(&wg)
+// 	}
+
+// 	wg.Wait()
+// 	// උත්තරේ 1000 එන්න ඕනේ වුණාට, මේක Run කළාම එන්නේ 945, 980 වගේ අමුතු ගණන්!
+// 	fmt.Println("Final Counter:", counter)
+// }
+
+// using mutex
 
 package main
 
@@ -7,12 +37,19 @@ import (
 	"sync"
 )
 
-var counter int = 0 // හැමෝම පාවිච්චි කරන පොදු Variable එක
+var counter int = 0
+var mu sync.Mutex // 1. Mutex එකක් (යතුරක්) හදාගන්නවා
 
-func increment(wg *sync.WaitGroup) {
+func incrementSafe(wg *sync.WaitGroup) {
 	defer wg.Done()
-	// Goroutines 1000ක් එකම වෙලාවේ මේකට කඩාපනිනවා!
-	counter++ 
+
+	// 2. යතුරෙන් ලොක් කරනවා. දැන් වෙන කාටවත් මෙතනට එන්න බැහැ!
+	mu.Lock()
+
+	counter++ // මේක තමයි Critical Section එක (දැන් 100% Safe)
+
+	// 3. යතුර ආපහු දෙනවා. (බොහෝවිට මේකත් defer mu.Unlock() ලෙස ලියයි)
+	mu.Unlock()
 }
 
 func main() {
@@ -20,10 +57,10 @@ func main() {
 
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
-		go increment(&wg)
+		go incrementSafe(&wg)
 	}
 
 	wg.Wait()
-	// උත්තරේ 1000 එන්න ඕනේ වුණාට, මේක Run කළාම එන්නේ 945, 980 වගේ අමුතු ගණන්!
-	fmt.Println("Final Counter:", counter) 
+	// දැන් අනිවාර්යයෙන්ම කිසිම වරදක් නැතුව 1000 ම එනවා!
+	fmt.Println("Final Counter:", counter)
 }
