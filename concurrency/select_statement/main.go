a385ee06cb9c455bdefba226eb813acc93b8a84a
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-08 14:25:02 +0530

    select with defualt case

--- concurrency/select_statement/main.go
+++ concurrency/select_statement/main.go
@@ -1,32 +1,55 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func server1(ch chan string) {
+// 	time.Sleep(2 * time.Second) // තත්පර 2ක් ගන්නවා
+// 	ch <- "Server 1 රිසල්ට් එක"
+// }
+
+// func server2(ch chan string) {
+// 	time.Sleep(1 * time.Second) // තත්පර 1ක් ගන්නවා (මේක වේගවත්)
+// 	ch <- "Server 2 රිසල්ට් එක"
+// }
+
+// func main() {
+// 	ch1 := make(chan string)
+// 	ch2 := make(chan string)
+
+// 	go server1(ch1)
+// 	go server2(ch2)
+
+// 	// මෙතනදී අපි Channels දෙකම දිහා එකපාර බලන් ඉන්නවා
+// 	select {
+// 	case msg1 := <-ch1:
+// 		fmt.Println("ආවේ 1 වෙනි එකෙන්:", msg1)
+// 	case msg2 := <-ch2:
+// 		fmt.Println("ආවේ 2 වෙනි එකෙන්:", msg2) // Server 2 වේගවත් නිසා මේක Print වේවි.
+// 	}
+// }
+
+// select with default
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func server1(ch chan string) {
-	time.Sleep(2 * time.Second) // තත්පර 2ක් ගන්නවා
-	ch <- "Server 1 රිසල්ට් එක"
-}
-
-func server2(ch chan string) {
-	time.Sleep(1 * time.Second) // තත්පර 1ක් ගන්නවා (මේක වේගවත්)
-	ch <- "Server 2 රිසල්ට් එක"
-}
+import "fmt"
 
 func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
+	ch := make(chan string)
 
-	go server1(ch1)
-	go server2(ch2)
+	// අපි මෙතනදී කිසිම Background Goroutine එකක් හදලා නැහැ.
+	// ඒ නිසා ch එකට Data එන්න විදිහක් ඇත්තෙම නැහැ.
 
-	// මෙතනදී අපි Channels දෙකම දිහා එකපාර බලන් ඉන්නවා
 	select {
-	case msg1 := <-ch1:
-		fmt.Println("ආවේ 1 වෙනි එකෙන්:", msg1)
-	case msg2 := <-ch2:
-		fmt.Println("ආවේ 2 වෙනි එකෙන්:", msg2) // Server 2 වේගවත් නිසා මේක Print වේවි.
+	case msg := <-ch:
+		// Data තිබුණොත් මේක වැඩ කරනවා
+		fmt.Println("Data හම්බුණා:", msg)
+	default:
+		// Data නැත්නම්, Block වෙලා Deadlock වෙන්නේ නැතුව කෙලින්ම මේක Run වෙනවා!
+		fmt.Println("Channel එකේ Data නැහැ. මම Block වෙන්නේ නැතුව පහළට යනවා.")
 	}
+
+	fmt.Println("Program එක සාර්ථකව අවසන් වුණා.")
 }
