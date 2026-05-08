a96bd819aa9b32c9be9a7530ed739393536b3ee6
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-05-08 14:27:46 +0530

    select with timeout

--- concurrency/select_statement/main.go
+++ concurrency/select_statement/main.go
@@ -32,24 +32,55 @@
 // }
 
 // select with default
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ch := make(chan string)
+
+// 	// අපි මෙතනදී කිසිම Background Goroutine එකක් හදලා නැහැ.
+// 	// ඒ නිසා ch එකට Data එන්න විදිහක් ඇත්තෙම නැහැ.
+
+// 	select {
+// 	case msg := <-ch:
+// 		// Data තිබුණොත් මේක වැඩ කරනවා
+// 		fmt.Println("Data හම්බුණා:", msg)
+// 	default:
+// 		// Data නැත්නම්, Block වෙලා Deadlock වෙන්නේ නැතුව කෙලින්ම මේක Run වෙනවා!
+// 		fmt.Println("Channel එකේ Data නැහැ. මම Block වෙන්නේ නැතුව පහළට යනවා.")
+// 	}
+
+// 	fmt.Println("Program එක සාර්ථකව අවසන් වුණා.")
+// }
+
+
+// select with timeout
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func slowDatabase(ch chan string) {
+	// තත්පර 5ක් යනවා Data එවන්න
+	time.Sleep(5 * time.Second) 
+	ch <- "User Data"
+}
 
 func main() {
 	ch := make(chan string)
+	go slowDatabase(ch)
 
-	// අපි මෙතනදී කිසිම Background Goroutine එකක් හදලා නැහැ.
-	// ඒ නිසා ch එකට Data එන්න විදිහක් ඇත්තෙම නැහැ.
+	fmt.Println("Data එනකම් බලන් ඉන්නවා (උපරිම තත්පර 3යි)...")
 
 	select {
-	case msg := <-ch:
-		// Data තිබුණොත් මේක වැඩ කරනවා
-		fmt.Println("Data හම්බුණා:", msg)
-	default:
-		// Data නැත්නම්, Block වෙලා Deadlock වෙන්නේ නැතුව කෙලින්ම මේක Run වෙනවා!
-		fmt.Println("Channel එකේ Data නැහැ. මම Block වෙන්නේ නැතුව පහළට යනවා.")
+	case result := <-ch:
+		fmt.Println("සාර්ථකයි! Data ආවා:", result)
+	
+	// time.After එකෙන් තත්පර 3ක් ගිය ගමන් මේ case එකට Signal එකක් එනවා
+	case <-time.After(3 * time.Second):
+		fmt.Println("❌ Timeout Error: තත්පර 3ක් ගියා, Data ආවේ නැහැ. වැඩේ නවත්තනවා!")
 	}
-
-	fmt.Println("Program එක සාර්ථකව අවසන් වුණා.")
 }
